bfaee1ef3dc865b407846b4f0893332f78820e25
Daniel S <tracid2008@googlemail.com>
2011-12-21 13:18:15 +0100

    unit test batch

--- tests/test.bat
+++ tests/test.bat
@@ -1,1 +1,6 @@
-phpunit --bootstrap c:\xampp\htdocs\shift1\tests\bootstrap.php  c:\xampp\htdocs\shift1\tests\Shift1\View\ViewTest.php
+@echo off
+if "%PHP_PEAR_BIN_DIR%" == "" set PHP_PEAR_BIN_DIR=C:\xampp\php
+GOTO RUN
+
+:RUN
+%PHP_PEAR_BIN_DIR%\phpunit -v
